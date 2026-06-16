8abab3a801d613fdff32c8995229c9ee922e6a9b
Kenneth <knthyang@gmail.com>
2026-06-16 00:06:49 -0400

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,25 @@
 import fapc from '../assets/vite.svg'
+import { Link } from 'react-router-dom'
 
 function Footer() {
 	return (
 		<footer className='min-w-full, min-h-150 bg-slate-500 flex flex-col px-20'> 
 			<img className='w-50 h-50' src={fapc} alt='logo'/>
+			
+			<div className='gap-20 flex flex-row items-center'>
+				<div className='flex flex-col gap-3 text-white tracking-tighter'>
+					<h1 className='text-3xl '> Services </h1>
+					<Link to='#services'> <h1 className='text-2xl opacity-70'> Accounting & Bookkeeping </h1> </Link>
+				</div>
+				<div className='flex flex-col gap-3 text-white tracking-tighter'>
+					<h1 className='text-3xl '> Company </h1>
+					<Link to='#services'> <h1 className='text-2xl opacity-70'> Accounting & Bookkeeping </h1> </Link>
+				</div>
+				<div className='flex flex-col gap-3 text-white tracking-tighter'>
+					<h1 className='text-3xl '> Connect </h1>
+					<Link to='#services'> <h1 className='text-2xl opacity-70'> Accounting & Bookkeeping </h1> </Link>
+				</div>
 
-			<div className='flex flex-col'>
-				<h1 className='text-3xl'> Services </h1>
-				<h1 className='text-3xl'> Services </h1>
 			</div>
 		</footer>
 	)
